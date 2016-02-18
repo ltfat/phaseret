9317d41d7431468d21d51b58bb49cbe9dbd2d88c
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-02-18 17:47:09 +0100

    Added complex2split to mex_helper.h

--- mex/mex_helper.h
+++ mex/mex_helper.h
@@ -1,6 +1,7 @@
 #ifndef _mex_helper_h
 #define _mex_helper_h
 
+#include <complex.h>
 #ifdef __STDC_UTF_16__
 #include <uchar.h>
 #endif
@@ -9,5 +10,16 @@
 #ifndef UNUSED
 #define UNUSED(expr) do { (void)(expr); } while (0)
 #endif
+
+void
+complex2split(const complex double* in, int L, double* outr, double* outi)
+{
+    for (int ii = 0; ii < L; ++ii)
+    {
+        outr[ii] = creal(in[ii]);
+        outi[ii] = cimag(in[ii]);
+    }
+
+}
 
 #endif
