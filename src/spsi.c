e3ce9e350ae5fac8c610ca63054fee90812cb951
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-02-18 17:24:50 +0100

    Fixed C impl. of spsi

--- src/spsi.c
+++ src/spsi.c
@@ -2,8 +2,8 @@
 #include "float.h"
 
 void
-spsireal(const double* s, int a, int M, int N, complex double* c,
-         double* initphase)
+spsireal(const double* s, int a, int M, int N,  double* initphase,
+         complex double* c)
 {
     int M2 = M / 2 + 1;
 
@@ -21,42 +21,7 @@ spsireal(const double* s, int a, int M, int N, complex double* c,
         const double* scol = s + n * M2;
         complex double* ccol = c + n * M2;
 
-        for (int m = 1; m < M2 - 1; m++)
-        {
-            if (scol[m] > scol[m - 1] && scol[m] > scol[m + 1])
-            {
-                double p;
-                double alpha = log(scol[m - 1] + DBL_MIN);
-                double beta = log(scol[m] + DBL_MIN);
-                double gamma = log(scol[m + 1] + DBL_MIN);
-                double denom = alpha - 2.0 * beta + gamma;
-
-                if (denom != 0.0)
-                    p = 0.5 * (alpha - gamma) / denom;
-                else
-                    p = 0;
-
-                double instf = m + p;
-                double peakPhase = tmpphase[m] + 2.0 * M_PI * a * instf / M;
-                tmpphase[m] = peakPhase;
-                // Go towards low frequency bins
-                int bin = m - 1;
-
-                while (bin > 0 && scol[bin] < scol[bin + 1])
-                {
-                    tmpphase[bin] = peakPhase;
-                    bin--;
-                }
-
-                bin = m + 1;
-
-                while (bin < M2 - 1 && scol[bin] < scol[bin - 1])
-                {
-                    tmpphase[bin] = peakPhase;
-                    bin++;
-                }
-            }
-        }
+        spsirealupdate(scol, a, M, tmpphase);
 
         for (int m = 0; m < M2; m++)
             ccol[m] = scol[m] * cexp(I * tmpphase[m]);
@@ -66,131 +31,82 @@ spsireal(const double* s, int a, int M, int N, complex double* c,
         free(tmpphase);
 }
 
-
-
 void
-spsireal_split(const double* cr, const double* ci, int a, int M, int N,
-               double* mask, double* coutr, double* couti)
+maskedspsireal(const double* s, int a, int M, int N,
+               const double* mask, const double* phase, double* initphase, complex double* c)
 {
     int M2 = M / 2 + 1;
-    double* cabsPtr;
-    double* tmpphase = aligned_alloc(ALIGNBYTES, M2 * sizeof * tmpphase);
-    memset(tmpphase, 0, M2 * sizeof * tmpphase);
 
-    if (ci == NULL)
-    {
-        cabsPtr = (double*)cr;
-    }
-    else
-    {
-        cabsPtr = aligned_alloc(ALIGNBYTES, M2 * N * sizeof * cabsPtr);
+    double* tmpphase = initphase;
 
-        for (int w = 0; w < M2 * N; w++)
-        {
-            cabsPtr[w] = cabs(cr[w] + I * ci[w]);
-        }
+    if (!initphase)
+    {
+        tmpphase = aligned_alloc(ALIGNBYTES, M2 * sizeof * tmpphase);
+        memset(tmpphase, 0, M2 * sizeof * tmpphase);
     }
+
 
     for (int n = 0; n < N; n++)
     {
-        const double* crcol = cr + n * M2;
-        const double* cicol = ci + n * M2;
-        double* cabscol = cabsPtr + n * M2;
-        double* coutrcol = coutr + n * M2;
-        double* couticol = couti + n * M2;
+        const double* scol = s + n * M2;
+        complex double* ccol = c + n * M2;
+        const double* maskcol = mask + n * M2;
+        const double* phasecol = phase + n * M2;
 
-        for (int m = 1; m < M2 - 1; m++)
-        {
-            if (cabscol[m] > cabscol[m - 1] && cabscol[m] > cabscol[m + 1])
-            {
-                double p;
-                double alpha = cabscol[m - 1];
-                double beta = cabscol[m];
-                double gamma = cabscol[m + 1];
-                double denom = alpha - 2.0 * beta + gamma;
-                int bin;
-
-                if (denom != 0.0)
-                {
-                    p = 0.5 * (alpha - gamma) / denom;
-                }
-                else
-                {
-                    p = 0;
-                }
-
-                double instf = m + p;
-                double peakPhase = tmpphase[m] + 2.0 * M_PI * a * instf / M;
-                tmpphase[m] = peakPhase;
-
-
-                if (p > 0.0)
-                {
-                    bin = m + 1;
-                    tmpphase[bin] = peakPhase;
-                    bin = m - 1;
-
-                    while (bin > 0 && cabscol[bin] < cabscol[bin + 1])
-                    {
-                        tmpphase[bin] = peakPhase;
-                        bin--;
-                    }
-
-                    bin = m + 2;
-
-                    while (bin < M2 - 1 && cabscol[bin] < cabscol[bin - 1])
-                    {
-                        tmpphase[bin] = peakPhase;
-                        bin++;
-                    }
-                }
-                else if (p < 0.0)
-                {
-                    bin = m - 1;
-                    tmpphase[bin] = peakPhase;
-                    bin = m + 1;
-
-                    while (bin < M2 - 1 && cabscol[bin] < cabscol[bin - 1])
-                    {
-                        tmpphase[bin] = peakPhase;
-                        bin++;
-                    }
-
-                    bin = m - 2;
-
-                    while (bin > 0 && cabscol[bin] < cabscol[bin + 1])
-                    {
-                        tmpphase[bin] = peakPhase;
-                        bin--;
-                    }
-
-                }
-            }
-        }
-
-        if (mask)
-        {
-            double* maskcol = mask + n * M2;
-
-            for (int m = 0; m < M2; m++)
-            {
-                if (maskcol[m])
-                {
-                    tmpphase[m] = carg(crcol[m] + I * cicol[m]);
-                }
-            }
-        }
+        spsirealupdate(scol, a, M, tmpphase);
 
         for (int m = 0; m < M2; m++)
-        {
-            double complex tmpz = cabscol[m] * cexp(I * tmpphase[m]);
-            coutrcol[m] = creal(tmpz);
-            couticol[m] = cimag(tmpz);
-        }
-
+            if (maskcol[m])
+                ccol[m] = scol[m] * cexp(I * phasecol[m]);
+            else
+                ccol[m] = scol[m] * cexp(I * tmpphase[m]);
     }
 
-    if (cr != cabsPtr) free(cabsPtr);
+    if (!initphase)
+        free(tmpphase);
+}
 
-    free(tmpphase);
+void
+spsirealupdate(const double* scol, int a, int M, double* tmpphase)
+{
+    int M2 = M / 2 + 1;
+
+    for (int m = 1; m < M2 - 1; m++)
+    {
+        if (scol[m] > scol[m - 1] && scol[m] > scol[m + 1])
+        {
+            double p;
+            double alpha = log(scol[m - 1] + DBL_MIN);
+            double beta = log(scol[m] + DBL_MIN);
+            double gamma = log(scol[m + 1] + DBL_MIN);
+            double denom = alpha - 2.0 * beta + gamma;
+
+            if (denom != 0.0)
+                p = 0.5 * (alpha - gamma) / denom;
+            else
+                p = 0;
+
+            double instf = m + p;
+            double peakPhase = tmpphase[m] + 2.0 * M_PI * a * instf / M;
+            tmpphase[m] = peakPhase;
+            // Go towards low frequency bins
+            int bin = m - 1;
+
+            while (bin > 0 && scol[bin] < scol[bin + 1])
+            {
+                tmpphase[bin] = peakPhase;
+                bin--;
+            }
+
+            bin = m + 1;
+
+            while (bin < M2 - 1 && scol[bin] < scol[bin - 1])
+            {
+                tmpphase[bin] = peakPhase;
+                bin++;
+            }
+        }
+    }
+
+
 }
--- src/spsi.h
+++ src/spsi.h
@@ -1,16 +1,51 @@
 /** \addtogroup spsi
- *  @{
+ *  \{
+ *  \file
+ *  \author Zdeněk Průša
+ *  \date 1 Feb 2016
+ *  \brief SPSI header
+
+ *  
  */
 #ifndef _spsi_h
 #define _spsi_h
 #include "config.h"
 
+/** SPSI algorithm implementation
+ *  \param[in] s  M x N arrays, target magnitude
+ *  \param[in] a  Hop factor
+ *  \param[in] M  Number of frequency channels
+ *  \param[in] N  Number of time frames
+ *  \param[in,out] initphase Length M vector, [in] phase of -1 frame,
+ *                           [out] phase of [N-1] frame.
+ *                           Can be NULL, but then a memory allocation will occur.
+ *  \param[out] c M x N array of coefficients
+ */
 void
-spsireal_split(const double* cr, const double* ci, int a, int M, int L,
-         double* mask, double* coutr, double* couti);
+spsireal(const double* s, int a, int M, int N, double* initphase, complex double* c);
+
+/** Masked SPSI algorithm implementation
+ *
+ *  Works as spsireal() except arg(c[ii])=phase[ii] if mask[ii] evaluates to true.
+ *
+ *  \param[in] s  M x N arrays, target magnitude
+ *  \param[in] a  Hop factor
+ *  \param[in] M  Number of frequency channels
+ *  \param[in] N  Number of time frames
+ *  \param[in] mask M x N array, mask of coefficients with known phase
+ *  \param[in] phase M x N array, known phase
+ *  \param[in,out] initphase Length M vector, [in] phase of -1 frame,
+ *                           [out] phase of [N-1] frame.
+ *                           Can be NULL, but then a memory allocation will occur.
+ *  \param[out] c M x N array of coefficients
+ */
+void
+maskedspsireal(const double* s, int a, int M, int N,
+               const double* mask, const double* phase, double* initphase, complex double* c);
+
 
 void
-spsireal(const double* s, int a, int M, int L, complex double* c, double* initphase);
+spsirealupdate(const double* scol, int a, int M, double* tmpphase);
 
 #endif /* _spsi_h */
-/** @} */
+/** \} */
