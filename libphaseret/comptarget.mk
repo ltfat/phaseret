1202cfe855fcd6bd6b318109d351e6c1a3621734
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-10-12 17:23:29 +0200

    Remove libphaseret subdir

--- libphaseret/comptarget.mk
+++
@@ -1,8 +0,0 @@
-ifeq ($(COMPTARGET),debug)
-	# Do debug stuff here
-	CFLAGS += -O0 -g
-	CXXFLAGS += -O0 -g
-else
-	CFLAGS += -O2 -DNDEBUG
-	CXXFLAGS += -O2 -DNDEBUG
-endif
--- libphaseret/include/phaseret/api.h
+++
@@ -1,9 +0,0 @@
-#include "dgtrealwrapper.h"
-#include "gla.h"
-#include "legla.h"
-#include "pghi.h"
-#include "spsi.h"
-#include "rtpghi.h"
-#include "rtisila.h"
-#include "utils.h"
-
