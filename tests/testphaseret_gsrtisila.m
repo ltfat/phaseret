fbe17da3ebed6b9a585ad057b34ec654ad032df1
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-10-19 22:37:03 +0200

    testphaseret_gsrtisila updated such that it now compares diffrerent phase initialization

--- tests/testphaseret_gsrtisila.m
+++ tests/testphaseret_gsrtisila.m
@@ -4,9 +4,13 @@ function test_failed=testphaseret_gsrtisila
 test_failed = 0;
 
 f = cocktailparty;
-a = 256;
+a = 64;
 M = 1024;
-g = {'hamming',1024};
+gl = 512;
+h = 0.01;
+g = gabwin({'gauss','width',gl,'atheight',h,'inf'},a,M,10*gl);
+g = long2fir(g,gl);
+gamma = -pi/4*(gl)^2/log(h);
 
 for pcId = 1:2
     % Complex case
@@ -18,7 +22,7 @@ for pcId = 1:2
     c = tra(f);
     s = abs(c);
 
-    [chat]=gsrtisila(s,g,a,M,phaseconv);
+    [chat]=gsrtisila(s,g,a,M,phaseconv,'maxit',2,'lookahead',3);
 
     E = magnitudeerrdb(s,proj(chat));
     fail = '';
@@ -29,7 +33,7 @@ for pcId = 1:2
 
     fprintf('GSRTISILA %s W=%d E=%.2f %s\n',phaseconv,1,E,fail);
 
-    [chat]=gsrtisila(s,g,a,M,phaseconv,'maxit',20);
+    [chat]=gsrtisila(s,g,a,M,phaseconv,'unwrap','maxit',2,'lookahead',3);
 
     E = magnitudeerrdb(s,proj(chat));
     fail = '';
@@ -40,7 +44,18 @@ for pcId = 1:2
 
     fprintf('GSRTISILA %s W=%d E=%.2f %s\n',phaseconv,1,E,fail);
 
-    [chat]=gsrtisila(s,g,a,M,phaseconv,'maxit',1,'lookahead',0,'spsi');
+    [chat]=gsrtisila(s,g,a,M,phaseconv,'spsi','maxit',2,'lookahead',3);
+
+    E = magnitudeerrdb(s,proj(chat));
+    fail = '';
+    if E>-20
+        test_failed = test_failed + 1;
+        fail = 'FAILED';
+    end
+
+    fprintf('GSRTISILA lookahead=2 %s W=%d E=%.2f %s\n',phaseconv,1,E,fail);
+    
+    [chat]=gsrtisila(s,g,a,M,phaseconv,'rtpghi',gamma,'maxit',2,'lookahead',3);
 
     E = magnitudeerrdb(s,proj(chat));
     fail = '';
