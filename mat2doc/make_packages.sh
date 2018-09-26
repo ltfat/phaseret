d2723a6785d272eb684dd08105f95b8970568be3
Zdenek Prusa <zdenek.prusa@gmail.com>
2018-09-26 10:35:30 +0200

    Adjusted release package name

--- mat2doc/make_packages.sh
+++ mat2doc/make_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 make -C mex -f Makefile_crossmingw
-/home/susnak/dev/mat2doc/mat2doc.py . mat --tgz --unix --script=release.py
+/home/susnak/dev/mat2doc/mat2doc.py . mat --tgz --unix --script=release.py --packagename=phaseret-%s-src
 /home/susnak/dev/mat2doc/mat2doc.py . mat --zip --dos --addon=phaseret_win64 --packagename=phaseret-%s-win64 --script=release.py
 /home/susnak/dev/mat2doc/mat2doc.py . html
 
